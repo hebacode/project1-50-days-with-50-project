450ffd5ca107c2e3b1ef8672d27d39568ce22bc9
hebacode <hebabegzi@gmail.com>
2025-10-17 19:32:04 +0300

    Add files via upload
    Day # 1 of 50 days in 50 project.
    this project is build by using HTML for structure of page, CSS for styling content and Javascript for interactive the button ,paragraphs and so on .

---
+++ Dua-project/dua-gen.js
@@ -0,0 +1,111 @@
+const pickOne = [{
+  name: 'حَسْبُنَا اللَّهُ وَنِعْمَ الْوَكِيلُ',
+  
+  meaning: 'Sufficient for us is Allah, and [He is] the best Disposer of affairs.',
+  source: 'Sahih Muslim'
+},
+{
+  name: 'رَبَّنَا آتِنَا فِي الدُّنْيَا حَسَنَةً وَفِي الآخِرَةِ حَسَنَةً وَقِنَا عَذَابَ النَّارِ',
+ 
+  meaning: 'Our Lord, give us in this world [that which is] good and in the Hereafter [that which is] good and protect us from the punishment of the Fire.',
+  source: 'Surah Al-Baqarah, 2:201'
+},
+{
+  name: 'رَبَّنَا آتِنَا فِي الدُّنْيَا حَسَنَةً وَفِي الآخِرَةِ حَسَنَةً وَقِنَا عَذَابَ النَّارِ',
+  meaning: 'Our Lord, grant us good in this world and good in the Hereafter, and protect us from the punishment of the Fire.',
+  source : 'Al-Baqarah, 2:201'
+},
+{
+  name : 'رَبِّ اشْرَحْ لِي صَدْرِي وَيَسِّرْ لِي أَمْرِي',
+  meaning: 'My Lord, expand for me my breast [with assurance] and ease for me my task.',
+  source: 'Surah Ta-Ha, 20:25-26'
+},
+{
+  name: 'اللَّهُمَّ إِنِّي أَسْأَلُكَ الْهُدَى وَالتُّقَى وَالْعَفَافَ وَالْغِنَى',
+  meaning: 'O Allah, I ask You for guidance, piety, abstinence (chastity), and self-sufficiency (wealth). ',
+  source: 'sahih Muslim'
+},
+{
+  name: 'رَبِّ زِدْنِي عِلْمًا',
+  meaning: 'My Lord, increase me in knowledge.',
+  source: 'Surah Ta-Ha, 20:114'
+},
+{
+  name: 'اللَّهُمَّ إِنِّي أَعُوذُ بِكَ مِنَ الْهَمِّ وَالْحَزَنِ',
+  meaning: 'O Allah, I seek refuge in You from anxiety and sorrow.',
+  source: 'Sahih al-Bukhari'
+},
+{
+  name: 'رَبِّ اغْفِرْ لِي وَارْحَمْنِي وَاهْدِنِي وَعَافِنِي وَارْزُقْنِي',
+  meaning: 'My Lord, forgive me and have mercy upon me and guide me and give me health and provide for me.',
+  source: 'Surah Ta-Ha, 20:82'
+},
+{
+  name: 'اللَّهُمَّ إِنِّي أَسْأَلُكَ رِضَاكَ وَالْجَنَّةَ',
+  meaning: 'O Allah, I ask You for Your pleasure and Paradise.',
+  source: 'Sahih al-Jami'
+},
+{
+  name: 'رَبِّ نَجِّنِي مِنَ الْقَوْمِ الظَّالِمِينَ',
+  meaning : 'My Lord, save me from the wrongdoing people.',
+  source: 'Surah Ash-Shuara, 26:169'
+},
+{
+  name:'اللَّهُمَّ إِنِّي أَسْأَلُكَ الْجَنَّةَ وَأَعُوذُ بِكَ مِنَ النَّارِ',
+  meaning: 'O Allah, I ask You for Paradise and I seek refuge in You from the Fire.',
+  source: 'Sahih al-Jami'
+},
+{
+  name: 'رَبِّ اشْرَحْ لِي صَدْرِي وَيَسِّرْ لِي أَمْرِي وَاحْلُلْ عُقْدَةً مِنْ لِسَانِي يَفْقَهُوا قَوْلِي',
+  meaning: 'My Lord, expand for me my breast [with assurance] and ease for me my task and untie the knot from my tongue that they may understand my speech.',
+  source: 'Surah Ta-Ha, 20:25-28'
+},
+{
+  name: 'اللَّهُمَّ إِنِّي أَسْأَلُكَ عِلْمًا نَافِعًا وَرِزْقًا طَيِّبًا وَعَمَلًا مُتَقَبَّلًا',
+  meaning: 'O Allah, I ask You for beneficial knowledge, goodly provision, and acceptable deeds.',
+  source: 'Sunan At-Tirmidhi'
+},{
+  name: 'رَبِّ اجْعَلْنِي مُقِيمَ الصَّلَاةِ وَمِن ذُرِّيَّتِي رَبَّنَا وَتَقَبَّلْ دُعَاءِ',
+  meaning: 'My Lord, make me an establisher of prayer, and [many] from my descendants. Our Lord, and accept my supplication.',
+  source: 'Surah Ibrahim, 14:40'
+},
+{
+  name: 'اللَّهُمَّ إِنِّي أَعُوذُ بِكَ مِنْ عَذَابِ جَهَنَّمَ وَمِنْ عَذَابِ الْقَبْرِ',
+  meaning: 'O Allah, I seek refuge in You from the punishment of Hell and from the punishment of the grave.',
+  source: 'Sahih al-Bukhari'
+},
+{
+  name: 'رَبِّ هَبْ لِي مِن لَّدُنكَ ذُرِّيَّةً طَيِّبَةً إِنَّكَ سَمِيعُ الدُّعَاءِ',
+  meaning: 'My Lord, grant me from Yourself a good offspring. Indeed, You are the Hearer of supplication.',
+  source: 'Surah Aal-E-Imran, 3:38'
+},
+{
+  name: 'اللَّهُمَّ إِنِّي أَسْأَلُكَ الْعَفْوَ وَالْعَافِيَةَ فِي الدُّنْيَا وَالْآخِرَةِ',
+  meaning: 'O Allah, I ask You for forgiveness and well-being in this world and in the Hereafter.',
+  source: 'Sunan At-Tirmidhi'
+},
+{
+  name: 'رَبِّ أَدْخِلْنِي مُدْخَلَ صِدْقٍ وَأَخْرِجْنِي مُخْرَجَ صِدْقٍ وَاجْعَل لِّي مِن لَّدُنكَ سُلْطَانًا نَّصِيرًا',
+  meaning: 'My Lord, cause me to enter a sound entrance and to exit a sound exit and grant me from Yourself a supporting authority.',
+  source: 'Surah Al-Isra, 17:80'
+}
+];
+
+
+function generateDua () {
+
+const pickDua = Math.floor(Math.random() * pickOne.length);
+
+const dua = pickOne[pickDua];
+
+const duaHTMl = `
+
+  <p>${dua.name}</p>
+  <p>${dua.meaning}</p>
+  <p>${dua.source}</p>
+
+`
+document.querySelector('.js-show-dua').innerHTML = duaHTMl;
+
+}
+ 
